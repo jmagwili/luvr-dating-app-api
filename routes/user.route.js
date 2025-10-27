3cfc09514924105eabab38fc5f1ddca67fb23041
jmagwili <joshuamagwili@gmail.com>
2025-10-27 18:21:22 +0800

    added post request to create user

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getUsers } from "../controllers/user.controller.js";
+import { getUsers, createUser } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
+userRouter.post("/", createUser);
 
 export default userRouter;
 
