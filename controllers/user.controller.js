02bad59a7e94b4a1f372d571a8c2a44e682cb7a8
jmagwili <joshuamagwili@gmail.com>
2025-10-28 14:39:51 +0800

    created route to update user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -27,3 +27,17 @@ export const getUsers = async (req, res) => {
         res.status(500).json({ message: "Error fetching users", error: error.message });
     }
 };
+
+export const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updates = req.body;
+        const updatedUser = await userModel.findByIdAndUpdate(id, updates, { new: true });
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        res.status(500).json({ message: "Error updating user", error: error.message });
+    }
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { getUsers, createUser } from "../controllers/user.controller.js";
+import { getUsers, createUser, updateUser } from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
 userRouter.post("/", createUser);
+userRouter.post("/update/:id", updateUser);
 
 export default userRouter;
 
